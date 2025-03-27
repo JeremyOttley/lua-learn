fdc1326f6324ad252c6d89709e5b380b9b60f282
Jeremy Ottley <Gazaunga@gmail.com>
2025-03-27 11:59:20 -0400

    Update and rename user to user.lua

---
+++ user.lua
@@ -0,0 +1,8 @@
+-- Prompt the user for input
+io.write("Please enter some text: ")
+
+-- Read the input and store it in a variable
+local userInput = io.read()
+
+-- Print the input back to the console
+print("\nYou entered:", userInput)
